8084355924c28c65f8fad49e88f8fe00f505b0e2
Zack Low <zal9946@rit.edu>
2018-03-22 23:46:11 -0400

    Update name, title, and description in Hero
    - Use bootstrap grid for responsiveness in Hero
    - Add profile picture to hero

--- src/HeroContent.vue
+++ src/HeroContent.vue
@@ -1,10 +1,27 @@
 <template>
-  <div class="transparent-bg p-2">
-    <div>
-      <h1 class="name">Zack Low</h1>
-      <h2 class="title">Software Engineer</h2>
-      <h4>Passionate about building modern scalable web applications across the entire stack, with experience front-end engineering in JavaScript, API design and development, Database design and development, data modelling and DevOps.</h4>
-      <social-links class="social-icons inline"></social-links>
+  <div class="transparent-bg p-4">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-3 text-center">
+          <img v-bind:src="profile" class="profile img-fluid rounded-circle"></img>
+        </div>
+        <div class="col-sm-9 d-flex align-items-center">
+          <div class="d-block w-100">
+            <h1 class="name">Zack Low</h1>
+            <h2 class="title">Software Engineer</h2>
+          </div>
+        </div>
+      </div>
+      <div class="row mt-4">
+        <div class="col-xs-12">
+          <p class="h4 p-2">Passionate about building modern scalable web applications across the entire stack, with experience front-end engineering in JavaScript, API design and development, Database design and development, data modelling and DevOps.</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 text-center">
+          <social-links class="social-icons w-100"></social-links>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -12,11 +29,17 @@
 <script>
 import NavContainer from './NavbarTop.vue';
 import SocialLinks from './components/SocialLinks.vue'
+import profile from './media/prof.jpg';
 
 export default {
   components: {
     navContainer: NavContainer,
     socialLinks: SocialLinks
+  },
+  data() {
+    return {
+      profile
+    }
   }
 }
 </script>
@@ -36,6 +59,6 @@ export default {
   // display: flex;
   // flex-direction: column;
   // justify-content: center;
-  background-color: rgba(0, 0, 0, 0.3);
+  background-color: rgba(0, 0, 0, 0.65);
 }
 </style>
