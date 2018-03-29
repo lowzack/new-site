2c994971b3b9e9777d11f30943414fd54e7727cf
Zack Low <zal9946@rit.edu>
2018-03-29 19:51:29 -0400

    Added education info

---
+++ src/EducationContent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="p-4">
+    <h1 class="mt-1 border-bottom border-primary">Education</h1>
+    <div class="row justify-content-sm-center p-4">
+      <div class="col-sm-3 text-center d-flex align-items-center">
+        <img class="img-fluid mx-auto" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Rochester_Institute_of_Technology_seal.svg/1200px-Rochester_Institute_of_Technology_seal.svg.png"/>
+      </div>
+      <div class="col-sm-9 d-flex align-items-center">
+        <div class="d-block w-100 text-right">
+          <h2>Rochester Institute of Technology</h2>
+          <h3>Information Technology <small>2015</small></h3>
+          <h4>Web and Mobile Development <small>Concentration</small></h4>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+</style>
